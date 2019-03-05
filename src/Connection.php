618c7dfcfbb464db719462e4e80dfb1764b2d57f
Paul G <paul@icontrolwp.com>
2019-03-05 10:42:51 +0000

    alignt with base wrapper

--- src/Connection.php
+++ src/Connection.php
@@ -2,36 +2,40 @@
 
 namespace FernleafSystems\ApiWrappers\StatusCake;
 
+/**
+ * Class Connection
+ * @package FernleafSystems\ApiWrappers\StatusCake
+ */
 class Connection extends \FernleafSystems\ApiWrappers\Base\Connection {
 
-	/**
-	 * @var string
-	 */
-	protected $sUsername;
-
-	/**
-	 * @return array
-	 */
-	public function getAuthenticationAsHeaders() {
-		return array(
-			'API'      => $this->getApiKey(),
-			'Username' => $this->getUsername(),
-		);
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getUsername() {
-		return $this->sUsername;
-	}
+	const API_URL = 'https://app.statuscake.com/API/%s';
+	const API_VERSION = '';
 
 	/**
 	 * @param string $sUsername
 	 * @return $this
 	 */
 	public function setUsername( $sUsername ) {
-		$this->sUsername = $sUsername;
+		$this->account_id = $sUsername;
 		return $this;
+	}
+
+	/**
+	 * @deprecated
+	 * @return string
+	 */
+	public function getUsername() {
+		return $this->account_id;
+	}
+
+	/**
+	 * @deprecated
+	 * @return array
+	 */
+	public function getAuthenticationAsHeaders() {
+		return array(
+			'API'      => $this->api_key,
+			'Username' => $this->account_id,
+		);
 	}
 }
